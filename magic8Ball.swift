359fd1bcc130a5d4c03445a4352f1e7e64b3c851
Jason Perez <jasandper@icloud.com>
2021-01-23 23:45:58 -0600

    switch statements

---
+++ magic8Ball.swift
@@ -0,0 +1,32 @@
+var playerName = "Jason"
+var playerQuestion = "Will it rain tomorrow?"
+var randomNumber = Int.random(in: 1...9)
+var eightBall: String
+
+// print(randomNumber)
+
+switch randomNumber {
+  case 1: 
+  eightBall = "Yes - definitely"
+  case 2: 
+  eightBall = "It is decidedly so"
+  case 3: 
+  eightBall = "Without a doubt"
+  case 4:
+  eightBall = "Reply hazy, try again"
+  case 5:
+  eightBall = "Ask again later"
+  case 6: 
+  eightBall = "Better not tell you now"
+  case 7:
+  eightBall = "My sources say no"
+  case 8:
+  eightBall = "Outlook not so good"
+  case 9:
+  eightBall = "Very doubtful"
+  default:
+  eightBall = "Error"
+}
+
+print("\(playerName) asks: \(playerQuestion)")
+print("Magic 8 Ball's answer: \(eightBall)")
