9af53a3bd0a6010af517d01963705d7abd6643f0
Jason Perez <jasandper@icloud.com>
2021-03-08 17:42:20 -0600

    create func to get computers choice

--- rockPaperScissors.swift
+++ rockPaperScissors.swift
@@ -6,4 +6,17 @@ func getUserChoice (userInput: String) -> String {
     }
 }
 
-print(getUserChoice(userInput: "rock"))
+
+func getComputerChoice () -> String {
+    let randomNum = Int.random(in: 0...2)
+
+    switch randomNum {
+        case 0: return "rock"
+        case 1: return "paper"
+        case 2: return "scissors"
+        default: return "Something went wrong"
+    }
+}
+
+
+
